b48ed2bb375b3b7231910c7dbf88a2170e64fc0c
Olszewski <U6028107@ten.thomsonreuters.com>
2016-07-07 17:51:41 +0200

    Before origin push

--- src/main/code/com/olo/App.java
+++ src/main/code/com/olo/App.java
@@ -4,16 +4,7 @@ import code.com.olo.easydip.model.Injection;
 import code.com.olo.easydip.core.InjectionsDetector;
 import java.util.ArrayList;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        ArrayList<Injection> injections = InjectionsDetector.instance.detectInjections();
-        System.out.println("Injections: " + injections.toArray().length);
+public class App {
+    public static void main( String[] args ) {
     }
 }
